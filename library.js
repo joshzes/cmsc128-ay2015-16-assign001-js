a4100f34539d077fd2d738409b04d417d1df8c34
Cacai Esguerra <joshzerica@gmail.com>
2016-02-14 11:03:43 +0800

    Added proper documentation

--- library.js
+++ library.js
@@ -5,13 +5,13 @@
 */
 
 function numToWords(){
-	var num = document.forms["numToWordsInput"].elements["num"].value;
-	var words = '';
-	var digit;
-	var is_teens = 0;
+	var num = document.forms["numToWordsInput"].elements["num"].value;	// get input from html form
+	var words = '';	// will hold output
+	var digit;	// will hold one digit
+	var is_teens = 0;	// if number is twelve, sixteen, seventeen, etc.
 
 	if(num == 0) {
-		words = 'Zero';
+		words = 'Zero';	// special case of zero
 		returnWord();
 	};
 
@@ -33,12 +33,12 @@ function numToWords(){
 		digit = Math.floor(num/10000);
 		num = num%10000;
 
-		if(digit == 1) is_teens = 1;
+		if(digit == 1) is_teens = 1;	// if 11, 12, 13, etc.
 		else words = words + getDigitWordTens(digit) + ' ';
 	}
 
-	if (num == 0 && is_teens == 0){
-		words = words + 'Thousand';
+	if (num == 0 && is_teens == 0){	// if the next digits are 0
+		words = words + 'Thousand';	// return output
 		returnWord();
 	}
 
@@ -46,22 +46,22 @@ function numToWords(){
 		digit = Math.floor(num/1000);
 		num = num%1000;
 
-		if (is_teens == 1) words = words + getWordTeen(digit) + ' Thousand ';
+		if (is_teens == 1) words = words + getWordTeen(digit) + ' Thousand ';	// if 11, 12, 13
 		else words = words + getDigitWord(digit) + ' Thousand ';
 		
-		if (num == 0) returnWord();
+		if (num == 0) returnWord();	// if next digits are 0, return output
 	}
 
-	is_teens = 0;
+	is_teens = 0;	// reset is_teens for the tens place
 
-	if(num/100 >= 1){ // digit is at thousands place
+	if(num/100 >= 1){ // digit is at hundreds place
 		digit = Math.floor(num/100);
 		num = num%100;
 		words = words + getDigitWord(digit) + ' Hundred ';
 		if (num == 0) returnWord();
 	}
 
-	if(num/10 >= 1){ // digit is at thousands place
+	if(num/10 >= 1){ // digit is at tens place
 		digit = Math.floor(num/10);
 		num = num%10;
 
@@ -70,8 +70,8 @@ function numToWords(){
 		if (num == 0 && is_teens == 0) returnWord();
 	}
 
-	if(num >= 1  || is_teens == 1){ // digit is at thousands place
-		digit = parseInt(num);
+	if(num >= 1  || is_teens == 1){ // digit is at ones place
+		digit = parseInt(num);	// convert num to int
 		if (is_teens == 1) words = words + getWordTeen(digit);
 		else words = words + getDigitWord(digit);
 		returnWord();
@@ -79,10 +79,10 @@ function numToWords(){
 
 	function returnWord(){
 		window.alert(words);
-		throw new Error();	// will not return to parent function
+		throw new Error();	// so it will not return to parent function
 	}
 
-	function getDigitWord(digit){
+	function getDigitWord(digit){	// return equivalent words
 		switch(digit){
 			case 1: return 'One';
 			case 2: return 'Two';  
@@ -97,7 +97,7 @@ function numToWords(){
 		}
 	}
 
-	function getDigitWordTens(digit){
+	function getDigitWordTens(digit){	// return equivalent words
 		switch(digit){
 			case 2: return 'Twenty';  
 			case 3: return 'Thirty';  
@@ -111,7 +111,7 @@ function numToWords(){
 		}
 	}
 
-	function getWordTeen(digit){
+	function getWordTeen(digit){	// return equivalent words
 		switch(digit){  
 			case 1: return 'Eleven';
 			case 2: return 'Twelve';  
@@ -127,33 +127,33 @@ function numToWords(){
 	}
 }
 
-function wordsToNum(words_given, x){
+function wordsToNum(words_given, x){	// words_given is passed from another function, can be none; x is either 1 or 0, depending on words_given
 	var num = 0;
 	var hold = 0;
 	var word;
 	if(words_given == "none") word = document.forms["wordsToNumInput"].elements["word"].value;
-	else word = words_given;
-	var words = word.split(" ");
+	else word = words_given;	// if there are words given, the input will be from another function
+	var words = word.split(" ");	// split word into an array
 	var i;
 
-	if(words[0] == "zero"){
+	if(words[0] == "zero"){	// special case of zero
 		num = 0;
 	}
 
 	for (i = 0; i < words.length; i++){
-		hold = (getNum(words[i])) * 1;
-		if(words[i+1] == "million") hold = hold * 1000000;
+		hold = (getNum(words[i])) * 1;	// get the equivalent word
+		if(words[i+1] == "million") hold = hold * 1000000;	// get the place
 		else if(words[i+1] == "hundred" && (words[i+2]=="thousand"||words[i+3]=="thousand"||words[i+4]=="thousand")) hold = hold *100000;
 		else if(words[i+1] == "thousand" || words[i+2] == "thousand") hold = hold * 1000;
 		else if(words[i+1] == "hundred") hold = hold * 100;
-		num += hold;
+		num += hold;	// add to accumulator
 	}
 
-	if (x == 0) window.alert(num);
-	else return num;
+	if (x == 0) window.alert(num);	// if from html file, alert
+	else return num;	// else return to calling function
 
 	function getNum(word){
-		switch(word){
+		switch(word){	// return equivalent word
 			case "one": return 1;
 			case "two": return 2;
 			case "three": return 3;
@@ -191,19 +191,19 @@ function wordsToCurrency(){
 	var word = document.forms["wordsToCurrencyInput"].elements["word"].value;
 	var num = wordsToNum(word, 1);
 
-	window.alert(currency + num);
+	window.alert(currency + num);	// alert currency and the output of wordsToNum function
 }
 
 function numDelimited(){
 	var delimiter = document.forms["numDelimitedInput"].elements["delimiter"].value;
 	var jumps = document.forms["numDelimitedInput"].elements["jumps"].value;
-	jumps = jumps * 1;
+	jumps = jumps * 1;	// convert to int
 	var num = document.forms["numDelimitedInput"].elements["num"].value;
 
-	var temp1 = num.substr(0, num.length - jumps);
-	var temp2 = num.substr(num.length - jumps, num.length);
+	var temp1 = num.substr(0, num.length - jumps);	// get the substring
+	var temp2 = num.substr(num.length - jumps, num.length);	// get the substring
 
-	var num_delimited = temp1 + delimiter + temp2;
+	var num_delimited = temp1 + delimiter + temp2;	// and combine with the delimiter
 
-	window.alert(num_delimited);
+	window.alert(num_delimited); // alert output
 }
